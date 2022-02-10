45359db54fa2606c90e85ca923e9ae9bf4a8195f
Ramansh <sangalramansh@gmail.com>
2022-02-10 19:32:53 +0530

    Final commit after adding features

--- functions.py
+++ functions.py
@@ -9,4 +9,20 @@ def gcd(a,b):
         else:
             return gcd(a,b-a)
 
+
+def fact(num):
+    if(num==0 or num == 1):
+        return 1
+    else:
+        return num * fact(num-1)
+
+def sum(a,b):
+    return a+b
+
+def multiply(a,b):
+    return a*b
+
+print(fact(5))
 print(gcd(3,1))
+print(sum(3,4))
+print(multiply(5,6))
